5abbd6cb00e9c93b78d1972bea9abf84dd8f2052
dominic.brady@theinformationlab.co.uk <dominic.brady@theinformationlab.co.uk>
2025-01-15 09:26:11 +0000

    project configs set to materialize marts. refactored dim_customers to add staging models.

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,31 @@
+with customers as (
+    SELECT *
+    FROM {{ ref('stg_jaffle_shop__customers') }}
+),
+orders as (
+    SELECT *
+    FROM {{ ref('stg_jaffle_shop__orders') }}
+),
+customer_orders as (
+    SELECT 
+        customer_id,
+        MIN(order_date) AS first_order_date,
+        MAX(order_date) AS most_recent_order_date,
+        COUNT(order_id) AS number_of_orders
+    FROM orders
+    GROUP BY customer_id
+),
+final as (
+    SELECT 
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        customer_orders.number_of_orders as number_of_orders
+    FROM customers
+    LEFT JOIN customer_orders 
+    ON customers.customer_id = customer_orders.customer_id
+)
+SELECT *
+FROM final
